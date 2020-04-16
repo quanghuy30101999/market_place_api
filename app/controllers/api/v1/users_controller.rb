7ecc3d5d4a5f6dec0ae3e8140a004205fb58c975
danghanh <ldhanh1999@gmail.com>
2020-04-16 22:50:19 +0700

    index user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,6 +5,10 @@ class Api::V1::UsersController < ApplicationController
     render json: User.find(params[:id])
   end
 
+  def index
+    render json: User.all
+  end
+
   def create
     user = User.new(user_params)
     if user.save
@@ -33,6 +37,6 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:password, :password_confirmation, :email)
   end
 end
