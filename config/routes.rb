7ccbddbeac316e0f0773abf76e7eb3745cc66c24
quanghuy <huytran.301099@gmail.com>
2020-04-16 14:59:39 +0700

    Adds the user create endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,10 @@ require "api_constraints"
 
 MarketPlaceApi::Application.routes.draw do
   devise_for :users
-  # Api definition
-  namespace :api, defaults: { format: :json },
-                  constraints: { subdomain: "api" }, path: "/" do
-    scope module: :v1,
-          constraints: ApiConstraints.new(version: 1, default: true) do
-      # We are going to list our resources here
-      resources :users, :only => [:show]
+
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: "api" }, path: "/" do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      resources :users, :only => [:show, :create]
     end
   end
 end
